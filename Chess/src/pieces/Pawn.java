469bca288e7fd626ba4b3106e8297ca37a2fb338
AlecHubacher <61433423+AlecHubacher@users.noreply.github.com>
2021-01-14 19:07:33 -0600

    Update Pawn.java

--- Chess/src/pieces/Pawn.java
+++ Chess/src/pieces/Pawn.java
@@ -3,19 +3,42 @@ package pieces;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import Game.Square;
 import Game.Board;
+import Game.CheckMateDetection;
+import Game.Square;
 
 public class Pawn extends Piece{
-	private int count=0;
 	public ArrayList<Square> legalMoves = new ArrayList<Square>();
-	
-	public Pawn(Square[][] board, String imageFile, Square placement,String pieceColor) throws IOException {
-		super(board, imageFile,placement,pieceColor);
+	public Pawn(Square[][] board, String imageFile, Square placement,String pieceColor,String pieceType) throws IOException {
+		super(board, imageFile,placement,pieceColor,pieceType);
 		this.placement=placement;
 		this.pieceColor=pieceColor;
 		this.board=board;
 		
+	}
+	
+	private void isLegalMove(int row, int col, boolean isDiag)
+	{
+		if(detect.isKingInCheck())
+		{
+			if(detect.getPathToKing().contains(board[row][col]))
+			{
+				System.out.println("The square is legal");
+				legalMoves.add(board[row][col]);
+				return;
+			}
+		}
+		else if(board[row][col].getPiece() == null)//if square is empty
+		{
+			if(!isDiag)
+			{
+				legalMoves.add(board[row][col]);
+			}
+		}
+		else if(!(board[row][col].getPiece().getColor().equalsIgnoreCase(this.getColor())) && isDiag)//square has enemy team piece
+		{
+			legalMoves.add(board[row][col]);
+		}
 	}
 	
 	public ArrayList<Square> getLegalMoves()
@@ -23,85 +46,82 @@ public class Pawn extends Piece{
 		return legalMoves;
 	}
 	
+	private boolean inBounds(int row, int col)
+	{
+		return (row >= 0 && row <= 7 && col >= 0 && col <= 7) ? true : false;
+	}
 	
+	public void setFirstMoveDone()
+	{
+		firstMove = false;
+	}
 	public void populateLegalMoves() //account for check and en passant later
 	{
 		int row = placement.getRow();
 		int col = placement.getCol();
-		System.out.println("placement is: "+row+" "+col);
-		if(pieceColor=="black")
+		legalMoves.add(board[row][col]);
+		
+		if(board[row][col].getPiece().getColor().equalsIgnoreCase("white"))
 		{
+			if(inBounds(row-1,col))
+			{
+				isLegalMove(row-1, col, false);
+			}
 			if(firstMove)
 			{
-				if(board[row+2][col].getPiece()==null)
-				{
-					legalMoves.add(board[row+2][col]);
-				}
+				isLegalMove(row-2, col, false);
 			}
-			if(board[row+1][col].getPiece()==null)
+			if(inBounds(row-1, col+1))
 			{
-				legalMoves.add(board[row+1][col]);
+				isLegalMove(row-1, col+1, true);
 			}
-			if(col<7)
+			if(inBounds(row-1, col-1))
 			{
-				if(board[row+1][col+1].getPiece()!=null&&board[row+1][col+1].getPiece().getColor().equalsIgnoreCase("white"))
-				{
-					legalMoves.add(board[row+1][col+1]);
-				}
-			}
-			if(col>0)
-			{
-				if(board[row+1][col-1].getPiece()!=null&&board[row+1][col-1].getPiece().getColor().equalsIgnoreCase("white"))
-				{
-					legalMoves.add(board[row+1][col-1]);
-				}
+				isLegalMove(row-1, col-1, true);
 			}
 		}
 		else
 		{
+			if(inBounds(row+1,col))
+			{
+				isLegalMove(row+1, col, false);
+			}
 			if(firstMove)
 			{
-				if(board[row-2][col].getPiece()==null)
-				{
-					legalMoves.add(board[row-2][col]);
-				}
+				isLegalMove(row+2, col, false);
 			}
-			if(board[row-1][col].getPiece()==null)
+			if(inBounds(row+1, col+1))
 			{
-				legalMoves.add(board[row-1][col]);
+				isLegalMove(row+1, col+1, true);
 			}
-			if(col<7)
+			if(inBounds(row+1, col-1))
 			{
-				if(board[row-1][col+1].getPiece()!=null&&board[row-1][col+1].getPiece().getColor().equalsIgnoreCase("white"))
-				{
-					legalMoves.add(board[row-1][col+1]);
-				}
-			}
-			if(col>0)
-			{
-				if(board[row-1][col-1].getPiece()!=null&&board[row-1][col-1].getPiece().getColor().equalsIgnoreCase("white"))
-				{
-					legalMoves.add(board[row-1][col-1]);
-				}
+				isLegalMove(row+1, col-1, true);
 			}
 		}
-		
 	}
 	
 	public void showLegalMoves()
 	{
-		for(int i=0;i<legalMoves.size();++i)
+		legalMoves.get(0).highlightMain();
+		for(int i=1;i<legalMoves.size();++i)
 		{
 			legalMoves.get(i).highlight();
 		}
 	}
 	
+	@Override
 	public void removeHighlights()
 	{
 		for(int i=0;i<legalMoves.size();++i)
 		{
-			legalMoves.get(i).newColor(4);//This resets highlights to origColor. For some reason resetColor does not work
+			legalMoves.get(i).makeOriginalColor();
 		}
+		
+	}
+	
+	public void clearLegalMoves()
+	{
 		legalMoves.clear();
 	}
 
