3cd28b9afc58e0d8862c23bac3355588ff2dc882
AlecHubacher <61433423+AlecHubacher@users.noreply.github.com>
2021-01-14 19:04:10 -0600

    Update Bishop.java

--- Chess/src/pieces/Bishop.java
+++ Chess/src/pieces/Bishop.java
@@ -4,113 +4,119 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import Game.Board;
+import Game.CheckMateDetection;
 import Game.Square;
 
 public class Bishop extends Piece{
 	public ArrayList<Square> legalMoves = new ArrayList<Square>();
+	private boolean nw = false;
+	private boolean sw = false;
+	private boolean ne = false;
+	private boolean se = false;
 	
-	public Bishop(Square[][] board,String imageFile, Square placement,String pieceColor) throws IOException {
-		super(board,imageFile, placement,pieceColor);
+	public Bishop(Square[][] board,String imageFile, Square placement,String pieceColor,String pieceType) throws IOException {
+		super(board,imageFile, placement,pieceColor,pieceType);
 		this.placement=placement;
 		this.pieceColor=pieceColor;
 		this.board=board;
 	}
-
 	
 	public ArrayList<Square> getLegalMoves()
 	{
 		return legalMoves;
 	}
+	
+	private void isLegalMove(int row, int col, int direction)
+	{
+		if(detect.isKingInCheck())
+		{
+			if(detect.getPathToKing().contains(board[row][col]))
+			{
+				System.out.println("The square is legal");
+				legalMoves.add(board[row][col]);
+				return;
+			}
+		}
+		else if(board[row][col].getPiece() == null)//if square is empty
+		{
+			System.out.println("this square is also legal");
+			legalMoves.add(board[row][col]);
+		}
+		else
+		{
+			if(!(board[row][col].getPiece().getColor().equalsIgnoreCase(this.getColor())))//square has enemy team piece
+			{
+				legalMoves.add(board[row][col]);
+			}
+			switch(direction)
+			{
+				case 1:
+					nw = true;
+					break;
+				case 2:
+					ne = true;
+					break;
+				case 3:
+					sw = true;
+					break;
+				case 4:
+					se = true;
+					break;
+			}
+		}
+	}
+	
+	private boolean inBounds(int row, int col)
+	{
+		return (row >= 0 && row <= 7 && col >= 0 && col <= 7) ? true : false;
+	}
+	
 	@Override
 	public void populateLegalMoves() 
 	{
-		boolean se=true;
-		boolean nw=true;
-		boolean sw=true;
-		boolean ne=true;
+		nw = false;
+		sw = false;
+		ne = false;
+		se = false;
+		
 		int row = placement.getRow();
 		int col = placement.getCol();
-		for(int i=1;i<8;++i)
+		legalMoves.add(board[row][col]);
+		int i = 1;
+		
+		//this function finds all legal moves for the north west diagonal
+		while(!nw && inBounds(row-i, col-i))
 		{
-			if(row+i<=7 && row+i>=0 && col+i<=7 && col+i>=0 && se)//south east checking if near edge of board
-			{
-				if(board[row+i][col+i].getPiece()==null || //if square is empty
-				!(placement.getPiece().getColor().equalsIgnoreCase(board[row+i][col+i].getPiece().getColor())))//if pieces are enemies
-				{
-					legalMoves.add(board[row+i][col+i]);
-					//This if statement checks for if current square being checked is enemy then end checking this diagonal for legalSquares
-					if(board[row+i][col+i].getPiece()!=null && //if square is not empty
-					!(placement.getPiece().getColor().equalsIgnoreCase(board[row+i][col+i].getPiece().getColor())))// if pieces are enemies
-					{
-						se=false;
-					}
-				}
-				else
-				{
-					se=false;
-				}
-			}
-			if(row-i<=7 && row-i>=0 && col-i<=7 && col-i>=0 && nw)//north west
-			{
-				if(board[row-i][col-i].getPiece()==null || 
-						!(placement.getPiece().getColor().equalsIgnoreCase(board[row-i][col-i].getPiece().getColor())))
-						{
-							legalMoves.add(board[row-i][col-i]);
-							if(board[row-i][col-i].getPiece()!=null && //if square is not empty
-									!(placement.getPiece().getColor().equalsIgnoreCase(board[row-i][col-i].getPiece().getColor())))// if pieces are enemies
-									{
-										nw=false;
-									}
-						}
-						else
-						{
-							nw=false;
-						}
-			}
-			if(row+i<=7 && row+i>=0 && col-i<=7 && col-i>=0 && sw)//south west
-			{
-				System.out.println("1");
-				if(board[row+i][col-i].getPiece()==null ||
-				!(placement.getPiece().getColor().equalsIgnoreCase(board[row+i][col-i].getPiece().getColor())))
-						
-						{
-					System.out.println("2");
-							legalMoves.add(board[row+i][col-i]);
-							if(board[row+i][col-i].getPiece()!=null && //if square is not empty
-									!(placement.getPiece().getColor().equalsIgnoreCase(board[row+i][col-i].getPiece().getColor())))// if pieces are enemies
-									{
-										sw=false;
-									}
-						}
-						else
-						{
-							sw=false;
-						}
-			}
-			if(row-i<=7 && row-i>=0 && col+i<=7 && col+i>=0 && ne)//north east
-			{
-				if(board[row-i][col+i].getPiece()==null || 
-						!(placement.getPiece().getColor().equalsIgnoreCase(board[row-i][col+i].getPiece().getColor())))
-						{
-							legalMoves.add(board[row-i][col+i]);
-							if(board[row-i][col+i].getPiece()!=null && //if square is not empty
-									!(placement.getPiece().getColor().equalsIgnoreCase(board[row-i][col+i].getPiece().getColor())))// if pieces are enemies
-									{
-										ne=false;
-									}
-						}
-						else
-						{
-							ne=false;
-						}
-			}
+			isLegalMove(row-i, col-i, 1);
+			i++;
 		}
+		i=1;
+		while(!ne && inBounds(row-i,col+i)) //this function finds all legal moves for the north east diagonal
+		{
+			isLegalMove(row-i, col+i, 2);
+			i++;
+		}
+		i=1;
+		while(!sw && inBounds(row+i,col-i)) //this function finds all legal moves for the south west diagonal
+		{
+			isLegalMove(row+i, col-i, 3);
+			i++;
+		}
+		i=1;
+		while(!se && inBounds(row+i,col+i)) //this function finds all legal moves for the south east diagonal
+		{
+			isLegalMove(row+i, col+i, 4);
+			i++;
+		}
+		
 	}
 
 	@Override
 	public void showLegalMoves() 
 	{
-		for(int i=0;i<legalMoves.size();++i)
+		
+		legalMoves.get(0).highlightMain();
+		for(int i=1;i<legalMoves.size();++i)
 		{
 			legalMoves.get(i).highlight();
 		}
@@ -122,10 +128,14 @@ public class Bishop extends Piece{
 	{
 		for(int i=0;i<legalMoves.size();++i)
 		{
-			legalMoves.get(i).newColor(4);
+			legalMoves.get(i).makeOriginalColor();
 		}
-		legalMoves.clear();
 		
+	}
+	
+	public void clearLegalMoves()
+	{
+		legalMoves.clear();
 	}
 
 }
